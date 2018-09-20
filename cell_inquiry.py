8ebe039786ec26d8419a15eb4d05ca5ef652c5f7
KayaanT <kayaan.tharani@gmail.com>
2018-09-20 15:37:02 -0600

    1

--- cell_inquiry.py
+++ cell_inquiry.py
@@ -3,3 +3,19 @@ CELL INQUIRY
 
 Ben & Kayaan
 """
+import time
+
+
+brain = [False, "Lungs"]
+blood = [True, "Oxygen"]
+lungs = [True, "Blood"]
+heart = [True, ""]
+
+while True:
+    if brain[0]:
+        lungs[0] = True
+    else:
+        lungs[0] = False
+    print(brain)
+    print(lungs)
+    time.sleep(1)
